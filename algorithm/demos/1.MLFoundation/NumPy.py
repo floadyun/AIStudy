0b2775b0e14b28ce0dee26b1cad9f6bd9457e292
floadyun <floadyun@163.com>
2018-03-30 16:43:23 +0800

    增加tensorflow学习demo

--- algorithm/demos/1.MLFoundation/NumPy.py
+++ algorithm/demos/1.MLFoundation/NumPy.py
@@ -43,15 +43,15 @@ invRandMat = randMat.I
 TraRandMat = randMat.T
 ArrRandMat = randMat.A
 # 输出结果
-print 'randArray=(%s) \n' % type(randArray), randArray
-print 'randMat=(%s) \n' % type(randMat), randMat
-print 'invRandMat=(%s) \n' % type(invRandMat), invRandMat
-print 'TraRandMat=(%s) \n' % type(TraRandMat), TraRandMat
-print 'ArrRandMat=(%s) \n' % type(ArrRandMat), ArrRandMat
+print('randArray=(%s) \n' % type(randArray), randArray)
+print('randMat=(%s) \n' % type(randMat), randMat)
+print('invRandMat=(%s) \n' % type(invRandMat), invRandMat)
+print('TraRandMat=(%s) \n' % type(TraRandMat), TraRandMat)
+print('ArrRandMat=(%s) \n' % type(ArrRandMat), ArrRandMat)
 # 矩阵和逆矩阵 进行求积 (单位矩阵，对角线都为1嘛，理论上4*4的矩阵其他的都为0)
 myEye = randMat*invRandMat
 # 误差
-print myEye - eye(4)
+print(myEye - eye(4))
 
 '''
 如果上面的代码运行没有问题，说明numpy安装没有问题
